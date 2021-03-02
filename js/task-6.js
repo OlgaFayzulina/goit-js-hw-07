fe87a2abc2a5b445b094d42a99020db1ba7b0835
Olga Fayzulina <o.fayzulina@gmail.com>
2021-03-02 18:50:00 +0200

    Fix task 6

--- js/task-6.js
+++ js/task-6.js
@@ -6,9 +6,8 @@ refs.input.addEventListener('blur', onInputCheckSymbolsQuantity);
 
 function onInputCheckSymbolsQuantity(event) {
     console.log(event.currentTarget.value.length);
-    refs.input.textContent = event.currentTarget.value.length;
      
-    if (event.currentTarget.value.length === 6) {
+    if (event.currentTarget.value.length === Number(refs.input.dataset.length)) {
         refs.input.classList.remove('invalid');
         refs.input.classList.add('valid');
     } else {
@@ -17,3 +16,4 @@ function onInputCheckSymbolsQuantity(event) {
 
     }
 }
+
